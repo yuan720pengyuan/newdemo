ef03ddcbaefa60a5aaf71c4c2d881d2d7029db1e
yuan720pengyuan <yuanpengyuan720@163.com>
2022-10-19 17:03:15 +0800

    dianji

---
+++ newdemo/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="isClick">
+      2222
+    </button>
+  </div>
+</template>
+
+<script setup>
+// @ is an alias to /src
+const isClick = () => {
+  alert('222222')
+}
+</script>
---
+++ newdemo/vue.config.js
@@ -0,0 +1,5 @@
+1
+const {defineConfig} = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
